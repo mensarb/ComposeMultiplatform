71ee119c36f42a299d6d8a33085d4f125f075728
dkoller <dominik.koller@myworld.com>
2025-02-05 12:15:22 +0100

    preparations

--- composeApp/src/commonMain/kotlin/org/example/composemultiplatform/App.kt
+++ composeApp/src/commonMain/kotlin/org/example/composemultiplatform/App.kt
@@ -7,28 +7,51 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import org.jetbrains.compose.resources.painterResource
-import org.jetbrains.compose.ui.tooling.preview.Preview
-
 import composemultiplatform.composeapp.generated.resources.Res
 import composemultiplatform.composeapp.generated.resources.compose_multiplatform
+import org.jetbrains.compose.resources.painterResource
+import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 @Preview
 fun App() {
     MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
-        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-            Button(onClick = { showContent = !showContent }) {
+        var showContent by remember {
+            mutableStateOf(false)
+        }
+
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .fillMaxWidth()
+        ) {
+            Button(onClick = {
+                showContent = !showContent
+            }) {
                 Text("Click me!")
             }
+
             AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
+                val greeting = remember {
+                    Greeting().greet()
+                }
+
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                ) {
+                    Image(
+                        painter = painterResource(Res.drawable.compose_multiplatform),
+                        contentDescription = null
+                    )
                     Text("Compose: $greeting")
                 }
             }
--- composeApp/src/iosMain/kotlin/org/example/composemultiplatform/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/org/example/composemultiplatform/MainViewController.kt
@@ -2,4 +2,6 @@ package org.example.composemultiplatform
 
 import androidx.compose.ui.window.ComposeUIViewController
 
-fun MainViewController() = ComposeUIViewController { App() }
+fun MainViewController() = ComposeUIViewController {
+    App()
+}
